99faecf932bca15ed46b574123b2e274706913f0
Sébastien Rousseau <sebrousseau33@gmail.com>
2015-01-27 13:45:47 -0500

    PileEstVide
    La fonction estVide est fonctionelle.

--- PileInt/pileInt.cpp
+++ PileInt/pileInt.cpp
@@ -1,3 +1,14 @@
 #include "PileInt.h"
 
+PileInt::PileInt()
+{
+	premierNoeud = NULL;
+}
 
+bool PileInt::estVide()
+{
+	if (premierNoeud == NULL){
+		return true;
+	}
+	return false;
+}
--- PileInt/pileInt.h
+++ PileInt/pileInt.h
@@ -2,13 +2,16 @@
 #define PILE_H_
 
 #include "noeudInt.h"
+#include <iostream>
+using namespace std;
 
 class PileInt
 {
   public:
-	     
+	  PileInt();
+	  bool estVide();
   private:
-        
+	  NoeudInt *premierNoeud;
 };
 
 #endif //PILE_H_
