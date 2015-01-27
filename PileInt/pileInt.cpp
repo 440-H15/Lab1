59c117227c951ac8bebbe2f9143904cd15cad813
Dominique Septembre <dominique.septembre@gmail.com>
2015-01-27 14:38:41 -0500

    depiler_un_int_devrait_le_supprimer_de_la_pile

--- PileInt/pileInt.cpp
+++ PileInt/pileInt.cpp
@@ -14,14 +14,17 @@ void PileInt::empiler(int _nombre)
 {
 	NoeudInt *nouveauNoeud = new NoeudInt();
 	nouveauNoeud->setContenu(_nombre);
-	nouveauNoeud->setPrecedent(nullptr);
+	nouveauNoeud->setPrecedent(this->sommetPile);
 
 	sommetPile = nouveauNoeud;
 }
 
 void PileInt::depiler()
 {
+	NoeudInt *aSupprimer = this->sommetPile;
+	this->sommetPile = aSupprimer->getPrecedent();
 
+	delete aSupprimer;
 }
 
 int PileInt::consulter()
