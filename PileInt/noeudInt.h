615f1d34518591131a6e2d64f5093ac68898ca7e
Alexandre Duclos <bass_player96@hotmail.com>
2015-01-27 13:07:47 -0500

    Méthodes
    Ajout des méthodes

--- PileInt/noeudInt.h
+++ PileInt/noeudInt.h
@@ -4,9 +4,13 @@
 class NoeudInt
 {
   public:
-	
+	  int getContenu();
+	  void setContenu(int _nombre);
+	  NoeudInt* getPrecedent();
+	  void setPrecedent(NoeudInt* _noeudPrecedent);
   private:
-	
+	  int contenu;
+	  NoeudInt* precedent;
 };
 
 #endif //NOEUDINT_H_
