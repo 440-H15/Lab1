b01b41be8c90998f9d57f6e4988738870268aa55
dominionized <dominique.begin.0@gmail.com>
2015-01-28 12:08:57 -0500

    Tout marche.

--- PileInt/pileInt.cpp
+++ PileInt/pileInt.cpp
@@ -1,3 +1,47 @@
 #include "PileInt.h"
+#include <system_error>
 
+PileInt::PileInt(){
+	noeudDessus = nullptr;
+}
 
+PileInt::~PileInt()
+{
+}
+
+bool PileInt::estVide()
+{
+	return (noeudDessus == nullptr);
+}
+
+void PileInt::empiler(int element)
+{
+	if (estVide())
+	{
+		noeudDessus = new NoeudInt(element, nullptr);
+		return;
+	}
+	else
+	{
+		NoeudInt *nouveauNoeudDessus = new NoeudInt(element, this->noeudDessus);
+		this->noeudDessus = nouveauNoeudDessus;
+	}
+}
+
+void PileInt::depiler()
+{
+	if (estVide())
+		throw std::runtime_error("Yo le cave tu peux pas dépiler une pile vide, lol.");
+
+	NoeudInt* noeudASupprimer = this->noeudDessus;
+	noeudDessus = noeudDessus->getPrecedent();
+	delete noeudASupprimer;
+}
+
+int PileInt::consulter()
+{
+	if (estVide())
+		throw std::runtime_error("La pile est vide bro.");
+	
+	return noeudDessus->getContenu();
+}
--- PileInt/pileInt.h
+++ PileInt/pileInt.h
@@ -6,9 +6,14 @@
 class PileInt
 {
   public:
-	     
+	  bool estVide();
+	  void empiler(int _nombre);
+	  void depiler();
+	  int consulter();
+	  PileInt();
+	  ~PileInt();
   private:
-        
+	  NoeudInt *noeudDessus;
 };
 
 #endif //PILE_H_
