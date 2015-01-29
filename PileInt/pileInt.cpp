5baaf88dc4b6fb5367ef18147ff19a38ffa4847f
Alexandre Duclos <bass_player96@hotmail.com>
2015-01-29 13:58:50 -0500

    ajout de méthode dans pileInt

--- PileInt/pileInt.cpp
+++ PileInt/pileInt.cpp
@@ -1,3 +1,24 @@
 #include "PileInt.h"
 
+PileInt::PileInt(){
+	sommetPile = nullptr;
+}
 
+bool PileInt:: estVide(){
+	
+	if (sommetPile != nullptr){
+		return false;
+	}
+
+	return true;
+}
+
+void PileInt::empiler(int _nombre){
+	NoeudInt* noeud;
+	for (int i = 0; i > _nombre; i++){
+		noeud = new NoeudInt();
+		noeud->setPrecedent(sommetPile);
+		noeud->setContenu(i);
+		sommetPile = noeud;
+	}
+}
