f9305c08bed751e3f01b216fffe177f49821f8b5
SamuelDaigle <samuel_daigle@outlook.com>
2015-01-30 08:07:10 -0500

    Added the files
    Je n'ai pas eu le temps de le faire à cause de problèmes d'installation,
    par contre j'ai aidé quelqu'un et il me l'a donné en retour.

--- PileInt/pileInt.cpp
+++ PileInt/pileInt.cpp
@@ -1,3 +1,61 @@
 #include "PileInt.h"
+#include "noeudInt.h"
+#include <iostream>
+
+
+using namespace std;
+
+PileInt::PileInt(){
+	sommetPile = nullptr;
+}
+
+bool PileInt::estVide(){
+	if (sommetPile == nullptr){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void PileInt::empiler(int _nombre){
+	if (estVide())
+	{
+		sommetPile = nullptr;
+	}
+	sommetPile = new NoeudInt(_nombre, sommetPile);
+}
+
+void PileInt::depiler(){
+
+
+
+	if (!estVide()){
+		NoeudInt* noeudAEnlever = sommetPile;
+		sommetPile = sommetPile->getNoeud();
+		delete noeudAEnlever;
+	}
+	else
+	{
+		throw runtime_error("Le pile est vide");
+	}
+
+
+}
+
+int PileInt::consulter()
+{
+	int nb = 0;
+	if (sommetPile != NULL)
+	{
+		nb = sommetPile->getNombre();
+	}
+	else
+	{
+		throw runtime_error("Le pile est vide");
+	}
+
+	return nb;
+}
 
 
