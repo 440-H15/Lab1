774618656b457d1985a968de791f99b6cec7378d
BigSmashy <geremy.sorlini.protic@gmail.com>
2015-01-30 13:41:09 -0500

    Every test was successful!

--- PileInt/pileInt.cpp
+++ PileInt/pileInt.cpp
@@ -1,4 +1,5 @@
 #include "PileInt.h"
+#include <stdexcept>
 
 PileInt::PileInt(){
 	sommetPile = nullptr;
@@ -14,16 +15,31 @@ bool PileInt::estVide(){
 
 void PileInt::empiler(int _nombre){ // Two cases: Empty and 1 item
 	NoeudInt *nouveauNoeud = new NoeudInt(_nombre);
-	if (estVide()){
-		sommetPile = nouveauNoeud;
-	}
-	else{
+	//if (estVide()){
+	//	sommetPile = nouveauNoeud;
+	//}
+	//else{
 		NoeudInt *sommetTemp = sommetPile;
 		sommetPile = nouveauNoeud;
 		nouveauNoeud->setPrecedent(sommetTemp);
-	}
+	//}
 }
 
-void PileInt::depiler(){ // Two cases: 1 item, and more than one.
+void PileInt::depiler(){
+	
+	if (estVide() == true) throw std::runtime_error("La pile est vide");
+	
+	NoeudInt* temp = sommetPile;
 
+	sommetPile = sommetPile->getPrecedent();
+
+	delete temp;
+	
+}
+
+int PileInt::consulter(){
+	
+	if (estVide() == true) throw std::runtime_error("La pile est vide");
+
+	return sommetPile->getContenu();
 }
