7e75c56000bbb23b140fe83320a0a5ac03062808
gabgagnon <gabgagnon1995@gmail.com>
2015-01-30 15:30:19 -0500

    remise lab1 Gabriel Gagnon

--- PileInt/noeudInt.h
+++ PileInt/noeudInt.h
@@ -4,9 +4,36 @@
 class NoeudInt
 {
   public:
-	
+	 NoeudInt(int _contenu,NoeudInt *_precedent); // construteur
+	 ~NoeudInt(); //destructeur
+	 NoeudInt getPrecedent();
+	 int getContenu();
   private:
-	
+	  NoeudInt *precedent;
+	  int contenu;
+
+
 };
+
+//constructeur
+NoeudInt::NoeudInt(int _valeurInterieur, NoeudInt *_noeudSommetActuellement){
+	precedent = _noeudSommetActuellement;
+	contenu = _valeurInterieur;
+
+}
+
+NoeudInt::~NoeudInt(){
+
+}
+
+NoeudInt NoeudInt::getPrecedent()
+{
+	return *precedent;
+}
+int NoeudInt::getContenu()
+{
+	return contenu;
+}
+
 
 #endif //NOEUDINT_H_
--- PileInt/pileInt.h
+++ PileInt/pileInt.h
@@ -3,12 +3,64 @@
 
 #include "noeudInt.h"
 
+using namespace std;
+
 class PileInt
 {
   public:
-	     
+	  PileInt(); // construteur
+	  ~PileInt();//destructeur
+	  bool estVide();
+	  void empiler(int);
+	  void depiler();
+	  int consulter();
+
   private:
+	  NoeudInt *sommetPile;
         
 };
+
+//constructeur
+ PileInt::PileInt(void){
+	sommetPile = nullptr;
+}
+ PileInt::~PileInt(){
+	 while (estVide()== false)
+	 {
+		 depiler();
+	 }
+ }
+
+
+ bool PileInt::estVide(){
+	bool estVide = true;
+
+	 if(sommetPile != nullptr){
+		estVide = false; 
+	 }
+	 return estVide;
+ }
+
+ void PileInt::empiler(int _valeurInterieur){
+	NoeudInt nouveauNoeud(_valeurInterieur, sommetPile);
+	*sommetPile = nouveauNoeud;
+ }
+ void PileInt::depiler(){
+	 if(estVide() == false){
+	NoeudInt *noeudSupprimer = sommetPile;
+	 *sommetPile = sommetPile->getPrecedent();
+	 delete noeudSupprimer;
+	 }
+ }
+
+ int PileInt::consulter(){
+	 if (sommetPile->getContenu()){
+		  return sommetPile->getContenu();
+	 }
+	 else
+	 {
+		// lancer l'Exception.
+	 }
+ }
 
 #endif //PILE_H_
