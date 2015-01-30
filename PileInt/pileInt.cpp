876641f934a5f30bd493147efebdfb885543dc86
Olivier Precourt <olivierprecourt@live.ca>
2015-01-30 08:00:00 -0500

    Code fini

--- PileInt/pileInt.cpp
+++ PileInt/pileInt.cpp
@@ -1,3 +1,50 @@
 #include "PileInt.h"
+#include "stdexcept"
 
+using namespace std;
 
+PileInt::PileInt(){
+	sommetPile = nullptr;
+}
+
+void PileInt::empiler(int _nombre){
+	NoeudInt* nouveauNoeud = new NoeudInt();
+	nouveauNoeud->setPrecedent(sommetPile);
+	nouveauNoeud->setContenu(_nombre);
+	sommetPile = nouveauNoeud;
+
+	nouveauNoeud = nullptr;
+	delete nouveauNoeud;
+}
+
+int PileInt::consulter(){
+	if (!estVide()){
+		return sommetPile->getContenu();
+	}
+	else {
+		throw runtime_error("La pile est vide");
+	}
+}
+
+void PileInt::depiler(){
+	if (!estVide()){
+		NoeudInt *temp = sommetPile->getPrecedent();
+		delete sommetPile;
+		sommetPile = temp;
+
+		temp = nullptr;
+		delete temp;
+
+	} else {
+		throw runtime_error("La pile est vide");
+	}
+}
+
+bool PileInt::estVide(){
+	if (sommetPile == nullptr){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
