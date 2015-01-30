2bb8ca7e8f1f1df4b16c263238f7c5be5e11eeed
anthonyvallee <tony.vallee@hotmail.com>
2015-01-30 11:01:25 -0500

    Stack Pop, Peek, Empty methods implemented
    The Stack class methods pop, peek, empty methods were implemented.

--- PileInt/pileInt.cpp
+++ PileInt/pileInt.cpp
@@ -22,16 +22,35 @@ void PileInt::push(int data)
 
 int PileInt::pop()
 {
-
+	if (firstNode) {
+		if (firstNode->getNextNode)
+		{
+			NoeudInt* tempNode = firstNode;
+			firstNode = firstNode->getNextNode();
+			delete tempNode;
+		}
+		else
+		{
+			delete firstNode;
+			firstNode = NULL;
+		}
+	}
 }
 
 int PileInt::peek()
 {
-
+	if (firstNode) {
+		return firstNode->getData();
+	}
+	return 0;
 }
 
 bool PileInt::empty()
 {
-	
+	if (firstNode)
+	{
+		return false;
+	}
+	return true;
 }
 
